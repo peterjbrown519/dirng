d04110ca2acec05eed461d4aa88a347b6fd8eade
Peter Brown <24820122+peterjbrown519@users.noreply.github.com>
2018-11-05 18:34:21 +0000

    updated example

--- dirng/examples/qubit_optimization.py
+++ dirng/examples/qubit_optimization.py
@@ -1,41 +1,36 @@
 """
-We extend the eb22 example and look at optimising the qubit system modelling the
+We extend the eb22.py example and look at optimising the qubit system modelling the
 devices.
 """
 import dirng as di
 from math import pi, log
 
-# Path to the sdpa solver
-SDPA_PATH = '/usr/userfs/p/pjb519/Dropbox/python/sdpa-7.3.8/'
+SOLVER = '/scratch/pjb519/sdpa-7.3.8/'
 
 
-
-
-# Quickly setting everything up
+# Quickly setting everything up again
 io_config = [[2,2], [2,2]]
 device_settings = 	{'name' : 'eb22',
 					'io_config' : io_config,
 					'relaxation_level' : 2,
 					'generation_inputs' : [0,0],
-					'sdpa_path' : SDPA_PATH}
+					'solver' : SOLVER}
+
 # Initializing the device
-devices = di.Devices(device_settings)
+dev = di.Devices(device_settings)
 
 # Adding the EB games
-eb_games = di.EBGames(io_config = io_config, distribution = None, delta = 0.001)
-devices.addGames(*eb_games)
+dev.games = di.EBGames(io_config = io_config, distribution = None, delta = 0.001)
 
 # Choosing initial qubit system angles (as before).
 system_angles = [pi/4, [0.0, pi/2], [pi/4, -pi/4]]
-
-# Let's modify the noise level's this time
+# Let's modify the noise level this time
 eta = 0.9
 vis = 0.99
-w = di.angles2Score(devices, system_angles, eta = eta, vis = vis)
-devices.setScore(w)
+dev.score = di.angles2Score(dev, system_angles, eta = eta, vis = vis)
 
 # Let's see how this has affected the devices.
-print('Before optimisation the devices\' min-entropy is {:.5f} bits'.format(devices.hmin))
+print('Before optimisation the devices\' min-entropy is {:.5f} bits'.format(dev.hmin))
 print('Using system angles:')
 print('theta = {:.5f}'.format(system_angles[0]))
 print('A0 = {:.5f}'.format(system_angles[1][0]))
@@ -48,7 +43,7 @@ print('B1 = {:.5f}'.format(system_angles[2][1]), end = '\n\n')
 
 
 """
-Given the detection efficiency, visibility  and the fact that we are not playing chsh,
+Given the detection efficiency, visibility (and the fact that we are not playing chsh),
 there may be a better choice of state and measurement angles that we could use.
 
 Using the method:
@@ -56,7 +51,7 @@ Using the method:
 We can look for better angles for our setup.
 """
 
-new_gp, new_angles = di.optimiseQubitGP(devices, system_angles, eta = eta, vis = vis)
+new_gp, new_angles = di.optimiseQubitGP(dev, system_angles, eta = eta, vis = vis)
 print('After optimising system angles, the devices\' min-entropy is now {:.5f} bits'.format(-log(new_gp,2)))
 print('theta = {:.5f}'.format(new_angles[0]))
 print('A0 = {:.5f}'.format(new_angles[1][0]))
