1de97517cb410c7eba4f5e7edbe271b317a97263
Peter Brown <24820122+peterjbrown519@users.noreply.github.com>
2018-11-05 17:58:45 +0000

    Updated __init__.py

--- dirng/__init__.py
+++ dirng/__init__.py
@@ -1,12 +1,15 @@
 from dirng.devices import Devices
 from dirng.games import Game
-from dirng.device_methods import cgExpressionReduction, EBGames, getScoreFromDistribution
+from dirng.device_methods import EBGames, distribution2Score
+from dirng.cg_methods import expression2CG, distribution2CG
 from dirng.protocol import Protocol
-from dirng.eat_methods import f_v, errV, errK, errW, eatBetaRate, eatRate, optimiseFminChoice
-from dirng.qubit_methods import qubitMeasurement, cgDistribution, angles2Score, angles2DualFunctional, angles2GP, optimiseQubitGP
+from dirng.eat_methods import f_v, errV, errK, errW, eatBetaRate, eatRate, gradEatRate, eatRateGA, optimiseFminChoice
+from dirng.qubit_methods import qubitMeasurement, distribution, angles2Score, angles2DualFunctional, angles2GP, optimiseQubitGP
 
-__all__ = ['Game', 'Devices',
-	'cgExpressionReduction', 'EBGames', 'getScoreFromDistribution',
+__all__ = ['Devices',
+	'Game',
+	'EBGames', 'distribution2Score',
+	'expression2CG', 'distribution2CG',
 	'Protocol',
-	'f_v', 'errV', 'errK', 'errW', 'eatBetaRate', 'eatRate', 'optimiseFminChoice',
-	'qubitMeasurement', 'cgDistribution', 'angles2Score', 'angles2DualFunctional', 'angles2GP', 'optimiseQubitGP']
+	'f_v', 'errV', 'errK', 'errW', 'eatBetaRate', 'eatRate', 'gradEatRate', 'eatRateGA', 'optimiseFminChoice',
+	'qubitMeasurement', 'distribution', 'angles2Score', 'angles2DualFunctional', 'angles2GP', 'optimiseQubitGP']
