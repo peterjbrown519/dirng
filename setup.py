0e52dcc6b545977fc5b92c5fcb3113b06d536d71
Peter Brown <24820122+peterjbrown519@users.noreply.github.com>
2018-11-05 17:53:44 +0000

    Updated setup.py
    Now writes a config.py file upon installation. 
    
    A default solver can be specified when installing by either
    - pip install dirng --install-option='--solver=/path/to/solver/'
    or using setup.py directly 
    - python setup.py install --solver=/path/to/solver/

--- setup.py
+++ setup.py
@@ -1,30 +1,51 @@
 from setuptools import setup
+from setuptools.command.install import install
+import os
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+
+class CustomInstallCommand(install):
+	"""Customized setuptools install command - allows you to pass solver path for defaulting."""
+	user_options = install.user_options + [
+	('solver=', None, 'Path to a solver.'),
+	]
+	def initialize_options(self):
+		install.initialize_options(self)
+		self.solver = ''
+
+	def finalize_options(self):
+		install.finalize_options(self)
+
+	def run(self):
+		with open(os.path.join(os.path.dirname(__file__), 'dirng', 'config.py'), 'w') as f:
+			f.write('DEFAULT_SOLVER_PATH = \'' + str(self.solver) + '\'\n')
+			f.write('SUPPORTED_SOLVERS = [\'sdpa\', \'sdpa_dd\', \'sdpa_qd\', \'sdpa_gmp\', \'sdpa.exe\']')
+
+		install.run(self)
 
 setup(name='dirng',
-      version='0.0.1',
-      author='Peter Brown',
-      author_email='pjb519@york.ac.uk',
-      description='Designing quantum-secure randomness expansion protocols',
-      long_description = readme(),
-      url='https://github.com/peterjbrown519/dirng',
-      license='MIT',
-      packages=['dirng'],
-      install_requires=[
-          'numpy',
+	  version='1.0.0',
+	  author='Peter J. Brown',
+	  author_email='pjb519@york.ac.uk',
+	  description='Designing quantum-secure randomness expansion protocols',
+	  long_description = 'For details see the github repo github.com/peterjbrown519/dirng',
+	  url='https://github.com/peterjbrown519/dirng',
+	  cmdclass={
+	  'install': CustomInstallCommand,
+		},
+	  license='GNU',
+	  packages=['dirng'],
+	  install_requires=[
+	  'numpy',
 	  'scipy',
 	  'tabulate',
 	  'ncpol2sdpa',
 	  'sympy',
 	  'tabulate',
 
-      ],
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
-        "Operating System :: OS Independent",
+	  ],
+	  classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+		"Operating System :: OS Independent",
 	"Topic :: Scientific/Engineering :: Physics",
-    ],)
+	],)
