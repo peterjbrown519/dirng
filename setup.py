0f44bdc01821d2da549777fe4d98c97bedf6ae9e
Peter Brown <24820122+peterjbrown519@users.noreply.github.com>
2018-11-06 17:01:40 +0000

    reverted setup.py
    Can't fix bug with passing extra arguments through to config file. User can set manually.

--- setup.py
+++ setup.py
@@ -1,32 +1,4 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-import pkg_resources
-import os
-import json
-
-
-
-class CustomInstallCommand(install):
-	"""Customized setuptools install command - allows you to pass solver path for defaulting."""
-	user_options = install.user_options + [
-	('solver=', None, 'Path to a solver.'),
-	]
-	def initialize_options(self):
-		install.initialize_options(self)
-		self.solver = ''
-
-	def finalize_options(self):
-		install.finalize_options(self)
-
-	def run(self):
-		manager = pkg_resources.ResourceManager()
-		config_file = manager.resource_filename('dirng','etc/dirng_config.json')
-		with open(config_file, 'r') as f:
-			cfg = json.load(f)
-		cfg["DEFAULT_SOLVER_PATH"] = self.solver
-		with open(config_file, 'w') as f:
-			json.dump(cfg, f)
-		install.run(self)
 
 setup(name='dirng',
 	  version='1.0.0',
@@ -35,9 +7,6 @@ setup(name='dirng',
 	  description='Designing quantum-secure randomness expansion protocols',
 	  long_description = 'For details see the github repo github.com/peterjbrown519/dirng',
 	  url='https://github.com/peterjbrown519/dirng',
-	  cmdclass={
-	  'install': CustomInstallCommand,
-		},
 	  license='GNU',
 	  packages= find_packages(),
 	  package_dir={'dirng': 'dirng'},
